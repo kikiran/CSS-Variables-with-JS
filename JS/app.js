5b669b6bbfb58e3945838459a6d476101de6f6b4
kikiran <kiran.m79955.com>
2024-02-22 17:23:33 +0530

    added JS functionality

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+const controls = document.querySelectorAll(".controls input");
+
+function handleChange() {
+	const suffix = this.dataset.sizing || " ";
+	document.documentElement.style.setProperty(
+		`--${this.name}`,
+		this.value + suffix
+	);
+}
+
+controls.forEach((input) => input.addEventListener("change", handleChange));
+controls.forEach((input) => input.addEventListener("mousemove", handleChange));
